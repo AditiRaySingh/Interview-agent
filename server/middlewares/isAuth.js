430f7a6910dc4d4b95605ec458d34580c55755fe
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-08-14 01:25:09 +0530

    Update isAuth.js

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,37 +1,37 @@
 import Jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
-    try {
-        const { token } = req.cookies;
+  try {
+    const { token } = req.cookies;
 
-        if (!token) {
-            return res.status(400).json({
-                message: "User does not have a token"
-            });
-        }
-
-        const verifyToken = Jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
-
-        if (!verifyToken) {
-            return res.status(400).json({
-                message: "User does not have a valid token"
-            });
-        }
-
-        req.userId = verifyToken.userId;
-
-        next();
-
-    } catch (error) {
-        console.log("isAuth error:", error);
-
-        return res.status(500).json({
-            message: `isAuth error: ${error.message}`
-        });
+    if (!token) {
+      return res.status(401).json({
+        message: "User is not authenticated",
+      });
     }
+
+    const verifyToken = Jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    if (!verifyToken) {
+      return res.status(401).json({
+        message: "Invalid or expired token",
+      });
+    }
+
+    req.userId = verifyToken.userId;
+
+    next();
+
+  } catch (error) {
+    console.log("isAuth error:", error);
+
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
+  }
 };
 
 export default isAuth;
