5aa40f51c5cd6104938bdae52e2eab387afda049
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-08-14 01:42:15 +0530

    Update AuthModel.jsx

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { FaTimes } from "react-icons/fa";
-import Auth from "./Auth.jsx";
+import Auth from "../Pages/Auth.jsx";
 
 function AuthModel({ onClose }) {
   const { userData } = useSelector((state) => state.user);
