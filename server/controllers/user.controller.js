335d987ab3db863d002f9c52feb3d6d80560d9b7
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-08-14 01:20:28 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,21 +1,26 @@
 
 import User from "../models/user.model.js"
 
-export const getCurrentUser=async(req,res)=>{
-    try{
-     
-        const userId=req.userId;
-        const user=await User.findById(userId);
-        if(!user)
-        {
-            return res.status(404).json({message:"user does not found"})
-        }
-        return res.status(200).json(user)
+export const getCurrentUser = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User does not exist",
+      });
     }
-    catch(error)
-    {
-       return res.status(500).json({message:'failed to get currentUser ${errro}'})
-    }
-}
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("Get current user error:", error);
+
+    return res.status(500).json({
+      message: `Failed to get current user: ${error.message}`,
+    });
+  }
+};
 
 export default getCurrentUser;
